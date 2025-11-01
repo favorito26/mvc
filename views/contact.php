0e78c13d979135999d5ceb68e79b60b9cfea391c
itncoffice <mufaddal2003@gmail.com>
2025-11-01 17:26:08 +0530

    initial commit

---
+++ views/contact.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<h1>Contact Us</h1>
+
+<form action="" method="post">
+    <div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">Email</label>
+  <input type="text" class="form-control" name="email" id="formGroupExampleInput" placeholder="Email ID">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label">Subject</label>
+  <input type="text" class="form-control" name="subject" id="formGroupExampleInput2" placeholder="Subject">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label">Body</label>
+  <textarea type="text" class="form-control" name="body" id="formGroupExampleInput2" placeholder="Body"></textarea>
+</div>
+<div class="mb-3">
+    <input type="submit" value="submit" class="form-control bg-primary text-white">
+</div>
+</form>
